c86527b804efe4e2a71eb818c978bf0fbc1c6bee
Matteo Mariot <matt.mariot72@gmail.com>
2021-08-24 15:58:17 +0200

    A simpler way to root the website

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,4 +1,8 @@
 class BandsController < ApplicationController
+  def index
+    @bands = Band.all
+  end
+
   def show
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
   def show
+    @bands_of_user = Member.where(user_id: current_user)
+
+    raise
   end
 end
