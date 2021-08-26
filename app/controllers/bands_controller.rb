f3ba5a2cf81874c065700dcfc047ffd7ef4a274a
Mohammed Tifour <medtifour@gmail.com>
2021-08-26 15:56:06 +0200

    sidebar fix and seed update

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,11 +1,10 @@
 class BandsController < ApplicationController
   def index
     @bands = Band.all
-    @user_bands = Member.where(user_id: current_user)
+
   end
 
   def show
-    @user_bands = Member.where(user_id: current_user)
     @songs = Song.where(band_id: Member.where(user_id: current_user))
   end
 
