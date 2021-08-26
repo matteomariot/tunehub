80e126e56fb5888cf08a6c12a6853284a4a92f00
Matteo Mariot <matt.mariot72@gmail.com>
2021-08-26 16:24:09 +0200

    Franchement le CRUD est long

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -4,23 +4,40 @@ class BandsController < ApplicationController
     @user_bands = Member.where(user_id: current_user)
   end
 
+  def new
+    @band = Band.new
+  end
+
+  def create
+    @band = Band.new(band_params)
+    @band.user_id = current_user.id
+    @band.save
+    redirect_to bands_path(@band)
+  end
+
   def show
     @user_bands = Member.where(user_id: current_user)
     @songs = Song.where(band_id: Member.where(user_id: current_user))
   end
 
-  def new
-  end
-
-  def create
-  end
-
   def edit
+    @band = Band.find(params[:id])
   end
 
   def update
+    @band = Band.find(params[:id])
+    @band.update(band_params)
+
+    # no need for app/views/restaurants/update.html.erb
+    redirect_to band_path(@band)
   end
 
   def destroy
+  end
+
+  private
+
+  def band_params
+    params.require(:band).permit(:name)
   end
 end
