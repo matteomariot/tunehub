e73ddedcd01b7b3a88e0a52062ab6283e611496a
Robin Dumont <robin.dumont@kedgebs.com>
2021-09-01 12:55:48 +0200

    setup model, controller and views for partitions audio records

--- app/models/partition.rb
+++ app/models/partition.rb
@@ -2,6 +2,7 @@ class Partition < ApplicationRecord
   belongs_to :song
   belongs_to :member
   has_many :comments, dependent: :destroy
+  has_one_attached :audio
 
   validates :title, length: { minimum: 2, too_short: "Partition title should contain atleast 2 characters" }
   validates :title, presence: { message: "Partition title is required" }
